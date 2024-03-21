3e8bc700dea86bb3fd91cb1a4d8d72bd9dcdf8f4
Omar Ahmed <omar01096567902@gmail.com>
2024-03-21 07:01:01 +0200

    Uploading Lab1

---
+++ C1_Variables_Operators/Lab1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+     printf("My name is Omar Ahmed \n I am student at fac of engineering \"Alex uni\" \n ");
+     return 0;
+}
+
+
---
+++ C1_Variables_Operators/Lab2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void)
+{    printf("Please enter a number: "); 
+     int x;
+     scanf(" %d",&x);  // enter a number
+     printf("you entered %d \n ",x); //print this number
+     return 0;
+}
+
